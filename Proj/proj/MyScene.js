8c72e484656ac7fdf7425b72ae3ac41e7bb0da4c
Askjeves <50593442+Askjeves@users.noreply.github.com>
2020-04-14 17:27:28 +0100

    Add files via upload

---
+++ Proj/proj/MyScene.js
@@ -0,0 +1,125 @@
+/**
+* MyScene
+* @constructor
+*/
+class MyScene extends CGFscene {
+    constructor() {
+        super();
+    this.texture = null;
+		this.appearance = null;
+
+    }
+    init(application) {
+        super.init(application);
+        this.initCameras();
+        this.initLights();
+
+        //Background color
+        this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+        this.gl.clearDepth(100.0);
+        this.gl.enable(this.gl.DEPTH_TEST);
+        this.gl.enable(this.gl.CULL_FACE);
+        this.gl.depthFunc(this.gl.LEQUAL);
+
+        this.setUpdatePeriod(50);
+
+        this.enableTextures(true);
+        this.appearance = new CGFappearance(this);
+        this.appearance.setAmbient(0.3, 0.3, 0.3, 1);
+        this.appearance.setDiffuse(0.7, 0.7, 0.7, 1);
+        this.appearance.setSpecular(0.0, 0.0, 0.0, 1);
+        this.appearance.setShininess(120);
+
+        this.texture = new CGFtexture(this, "./images/earth.jpg");
+        this.appearance.setTexture(this.texture);
+        this.appearance.setTextureWrap('REPEAT', 'REPEAT');
+
+        //this.appearanceUnitCube = new MyUnitCube(this);
+        // this.appearanceUnitCube = new CGFappearance(this);
+        // this.appearanceUnitCube.setAmbient(0.3, 0.3, 0.3, 1);
+        // this.appearanceUnitCube.setDiffuse(0.7, 0.7, 0.7, 1);
+        // this.appearanceUnitCube.setSpecular(0.0, 0.0, 0.0, 1);
+        // this.appearanceUnitCube.setShininess(120);
+        //
+        // this.textureUnitCube = new CGFtexture(this, "./images/cubemap.png");
+        // this.appearanceUnitCube.setTexture(this.textureUnitCube);
+        // this.appearanceUnitCube.setTextureWrap('CLAMP_TO_EDGE', 'CLAMP_TO_EDGE');
+
+
+        //Initialize scene objects
+        this.axis = new CGFaxis(this);
+        this.incompleteSphere = new MySphere(this, 16, 8);
+        this.myCylinder=new MyCylinder(this,6);
+        this.myUnitCube=new MyUnitCube(this);
+        //Objects connected to MyInterface
+        this.displayAxis = true;
+
+    }
+
+
+    initLights() {
+        this.lights[0].setPosition(15, 2, 5, 1);
+        this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
+        this.lights[0].enable();
+        this.lights[0].update();
+    }
+
+
+    initCameras() {
+        this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(15, 15, 15), vec3.fromValues(0, 0, 0));
+    }
+
+
+    setDefaultAppearance() {
+        this.setAmbient(0.2, 0.4, 0.8, 1.0);
+        this.setDiffuse(0.2, 0.4, 0.8, 1.0);
+        this.setSpecular(0.2, 0.4, 0.8, 1.0);
+        this.setShininess(10.0);
+    }
+
+
+
+
+    // called periodically (as per setUpdatePeriod() in init())
+    update(t){
+        //To be done...
+    }
+
+    display() {
+        // ---- BEGIN Background, camera and axis setup
+        // Clear image and depth buffer everytime we update the scene
+        this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+        // Initialize Model-View matrix as identity (no transformation
+        this.updateProjectionMatrix();
+        this.loadIdentity();
+        // Apply transformations corresponding to the camera position relative to the origin
+        this.applyViewMatrix();
+
+
+        // Draw axis
+        if (this.displayAxis)
+            this.axis.display();
+
+        //this.setDefaultAppearance();
+
+
+        // ---- BEGIN Primitive drawing section
+
+        //This sphere does not have defined texture coordinates
+        //this.incompleteSphere.display();
+        this.pushMatrix();
+        this.scale(5, 5, 5);
+        this.myUnitCube.appearance.apply();
+        this.myUnitCube.display();
+        this.popMatrix();
+
+
+        //this.myCylinder.display();
+
+
+
+        // ---- END Primitive drawing section
+    }
+}
---
+++ Proj/proj/MyUnitCube.js
@@ -0,0 +1,176 @@
+/**
+ * MyDiamond
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyUnitCube extends CGFobject {
+	constructor(scene) {
+		super(scene);
+		this.initBuffers();
+		this.initMaterials();
+	}
+	initBuffers() {
+		 this.vertices = [
+			      // Front Face
+			 			-0.5, -0.5,  0.5,
+	           0.5, -0.5,  0.5,
+	           0.5,  0.5,  0.5,
+	          -0.5,  0.5,  0.5,
+
+	          // Back face
+	          -0.5, -0.5, -0.5,
+	          -0.5,  0.5, -0.5,
+	           0.5,  0.5, -0.5,
+	           0.5, -0.5, -0.5,
+
+						 // Top face
+            -0.5,  0.5, -0.5,
+	          -0.5,  0.5,  0.5,
+	           0.5,  0.5,  0.5,
+	           0.5,  0.5, -0.5,
+
+	          // Bottom face
+	          -0.5, -0.5, -0.5,
+	           0.5, -0.5, -0.5,
+	           0.5, -0.5,  0.5,
+	          -0.5, -0.5,  0.5,
+
+	          // Right face
+	           0.5, -0.5, -0.5,
+	           0.5,  0.5, -0.5,
+	           0.5,  0.5,  0.5,
+	           0.5, -0.5,  0.5,
+
+	          // Left face
+	          -0.5, -0.5, -0.5,
+	          -0.5, -0.5,  0.5,
+	          -0.5,  0.5,  0.5,
+	          -0.5,  0.5, -0.5
+		];
+
+
+		this.normals = ([
+            // Front face
+            0, 0, 1,
+            0, 0, 1,
+            0, 0, 1,
+            0, 0, 1,
+
+            // Back face
+            0, 0, 1,
+            0, 0, 1,
+            0, 0, 1,
+            0, 0, 1,
+
+            // Top face
+            0, -1, 0,
+            0, -1, 0,
+            0, -1, 0,
+            0, -1, 0,
+
+            // Bottom face
+				            // 0, -1, 0,
+				            // 0, -1, 0,
+				            // 0, -1, 0,
+				            // 0, -1, 0,
+										// Bottom face
+
+						0, 1, 0,
+						0, 1, 0,
+						0, 1, 0,
+						0, 1, 0,
+
+            // Right face
+            1, 0, 0,
+            1, 0, 0,
+            1, 0, 0,
+            1, 0, 0,
+
+            // Left face
+            1, 0, 0,
+            1, 0, 0,
+            1, 0, 0,
+            1, 0, 0
+        ]);
+
+
+				this.indices = [
+									// 0,  1,  2,      0,  2,  3,    // front
+				          // 4,  5,  6,      4,  6,  7,    // back
+									 6,  5,  4,      7,  6,  4,    // inside back
+
+				          // 8,  9,  10,     8,  10, 11,   // top
+				           //12, 13, 14,     12, 14, 15,   // bottom
+									 14, 13, 12,     15, 14, 12,   // inside bottom
+
+				           //16, 17, 18,     16, 18, 19,   // right
+				           //20, 21, 22,     20, 22, 23,    // left
+									 22, 21, 20,     23, 22, 20    // inside left
+						];
+
+							this.texCoords =([
+					            // Front face
+											3/4, 2/3,
+											3/4, 1/3,
+											1.0, 1/3,
+											1.0, 2/3,
+
+					            // Back face
+											3/4, 2/3,
+											3/4, 1/3,
+											1.0, 1/3,
+											1.0, 2/3,
+
+											// Top face
+											1.0, 2/3,
+											3/4, 2/3,
+											3/4, 2/3,
+											3/4, 1/3,
+
+											// Bottom face
+											1/4, 2/3,
+											2/4, 2/3,
+											2/4, 1.0,
+											1/4, 1.0,
+
+											// Right face
+											3/4, 2/3,
+											3/4, 1/3,
+											1.0, 1/3,
+											1.0, 1/3,
+
+											// Left face
+											// 1/4, 2/3,
+											// 1/4, 1/3,
+											// 2/4, 1/3,
+											// 2/4, 2/3,
+
+											3/4, 2/3,
+											3/4, 1/3,
+											1.0, 1/3,
+											1.0, 2/3,
+
+					        ]);
+
+
+
+		//The defined indices (and corresponding vertices)
+		//will be read in groups of three to draw triangles
+		this.primitiveType = this.scene.gl.TRIANGLES;
+
+		this.initGLBuffers();
+	}
+	initMaterials() {
+
+		this.appearance = new CGFappearance(this.scene);
+		this.appearance.setAmbient(0.3, 0.3, 0.3, 1);
+		this.appearance.setDiffuse(0.7, 0.7, 0.7, 1);
+		this.appearance.setSpecular(0.0, 0.0, 0.0, 1);
+		this.appearance.setShininess(120);
+		this.appearance.loadTexture( "./images/cubemap.png");
+		this.appearance.setTextureWrap('CLAMP_TO_EDGE', 'CLAMP_TO_EDGE');
+
+
+			}
+
+}
